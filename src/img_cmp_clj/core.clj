9c314ce6a999c16471c647653c2b277bf661b178
apeckham <apeckham@gmail.com>
2016-04-19 21:26:26 -0700

    initial

---
+++ src/img_cmp_clj/core.clj
@@ -0,0 +1,7 @@
+(ns img-cmp-clj.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/img_cmp_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns img-cmp-clj.core-test
+  (:require [clojure.test :refer :all]
+            [img-cmp-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
