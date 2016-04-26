5c0b737ca165f725d3205bc9130efc9693ed0762
apeckham <apeckham@gmail.com>
2016-04-25 21:23:37 -0700

    print HTML that shows images, filenames and the output of the comparison

--- src/img_cmp_clj/core.clj
+++ src/img_cmp_clj/core.clj
@@ -1,13 +1,14 @@
 (ns img-cmp-clj.core
-  (:require [me.raynes.conch :refer [with-programs]])
+  (:require [me.raynes.conch :refer [with-programs]]
+            [hiccup.page :refer [html5 include-css include-js]])
   (:gen-class))
 
 (defn compare-files
-  [expected actual diff]
+  [{:keys [expected actual diff]}]
   (let [comparison (with-programs [compare]
                                   (compare "-metric" "AE" expected actual diff {:throw false :verbose true}))
         exit-code (-> comparison :exit-code deref)]
-    {:match (= 0 exit-code)
+    {:match   (= 0 exit-code)
      :message (-> comparison :proc :err first)}))
 
 (defn change-prefix
@@ -24,6 +25,35 @@
                        :diff (change-prefix % "diff/")
                        :actual (change-prefix % "actual/")))))
 
+(defn compare-all
+  []
+  (map #(merge % (compare-files %)) (expected-seq)))
+
+(defn render-item
+  [item]
+  [:div
+   [:h1
+    (:expected item)
+    ": "
+    (if (:match item) "Matched" "Did not match")]
+   [:p
+    [:code
+     (:message item)]]
+   [:table.table
+    [:tr
+     [:td.text-center [:img.img-responsive {:src (:expected item)}]]
+     [:td.text-center [:img.img-responsive {:src (:actual item)}]]
+     [:td.text-center [:img.img-responsive {:src (:diff item)}]]]]])
+
+(defn render
+  [items]
+  (html5
+    [:head
+     (include-css "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css")
+     (include-js "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js")
+     [:style "td { width: 33% }"]]
+    [:div.container (map render-item items)]))
+
 (defn -main
   [& args]
-  (println "Hello, World!"))
+  (->> (compare-all) render (spit "out.html")))
